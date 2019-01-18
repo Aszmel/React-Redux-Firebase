b3aa2e836fa536d75ccaf9e121152d590c45c49c
Aszmel <aszmel@o2.pl>
2019-01-18 20:36:35 +0100

    fixed destructuring

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,8 +1,12 @@
 const initState = {
   projects: [
-    { id: "1", title: "help me find peach", content: "blah blah blah" },
-    { id: "2", title: "collect all the stars", content: "blah blah blah" },
-    { id: "3", title: "egg hunt with aszmel", content: "blah blah blah" }
+    { id: "1", title: "help me find peach", content: "find peach to eat it!" },
+    {
+      id: "2",
+      title: "collect all the stars",
+      content: "to be Master of Stars"
+    },
+    { id: "3", title: "egg hunt with aszmel", content: "scrambled eggs!" }
   ]
 };
 
