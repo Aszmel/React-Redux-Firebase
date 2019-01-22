988c7c6bbde3f95640e9150695090c7964682870
Aszmel <aszmel@o2.pl>
2019-01-22 21:00:51 +0100

    syncing data with Firestore

--- src/index.js
+++ src/index.js
@@ -17,8 +17,8 @@ const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-    reduxFirestore(fbConfig),
-    reactReduxFirebase(fbConfig)
+    reactReduxFirebase(fbConfig),
+    reduxFirestore(fbConfig)
   )
 );
 
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,14 +1,4 @@
-const initState = {
-  projects: [
-    { id: "1", title: "help me find peach", content: "find peach to eat it!" },
-    {
-      id: "2",
-      title: "collect all the stars",
-      content: "to be Master of Stars"
-    },
-    { id: "3", title: "egg hunt with aszmel", content: "scrambled eggs!" }
-  ]
-};
+const initState = {};
 
 const projectReducer = (state = initState, action) => {
   switch (action.type) {
