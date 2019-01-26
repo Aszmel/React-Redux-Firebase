0f2cd31365c129ce340d3c0b2e95611a0c70d279
Aszmel <aszmel@o2.pl>
2019-01-26 20:48:09 +0100

    added logout action

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -10,10 +10,14 @@ const authReducer = (state = initState, action) => {
         authError: "Login failed"
       };
     case "LOGIN_SUCCESS":
+      console.log("login success");
       return {
         ...state,
         authError: null
       };
+    case "LOGOUT_SUCCESS":
+      console.log("signout success");
+      return state;
     default:
       return state;
   }
