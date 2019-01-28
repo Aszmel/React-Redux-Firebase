9858596fbb4839246abfe13b7fb06e5397a7f368
Aszmel <aszmel@o2.pl>
2019-01-28 20:45:14 +0100

    added props (from Firebase) to set profile accesible in state to get initials in component

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,14 +5,18 @@ import SignedOutLinks from "./SignedOutLinks";
 import { connect } from "react-redux";
 
 const Navbar = props => {
-  const { auth } = props;
-  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
+  const { auth, profile } = props;
+  const links = auth.uid ? (
+    <SignedInLinks profile={profile} />
+  ) : (
+    <SignedOutLinks />
+  );
   return (
     <div>
       <nav className="nav-wrapper grey darken-3">
         <div className="container">
           <Link to="/" className="brand-logo">
-            Share Your Story!
+            Share you story {props.profile.firstName}!
           </Link>
           {links}
         </div>
@@ -23,7 +27,8 @@ const Navbar = props => {
 
 const mapStateToProps = state => {
   return {
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    profile: state.firebase.profile
   };
 };
 
